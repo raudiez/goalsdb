c9767dba312b5e0b3f928b515676d40f52123ee7
Raúl Díez <rauldiez20@gmail.com>
2017-10-04 06:01:14 +0200

    Disclaimer updated.

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -7,13 +7,13 @@
 
 <p>La información contenida en esta web así como imágenes, logos y nombres tienen fin meramente de entretenimiento. Ninguna de las asociaciones u organizaciones mencionadas en esta web están debidamente registradas por tratarse de organizaciones o asociaciones ficticias.</p>
 
-<p>Los logos, nombres e imágenes utilizados no han sido contrastados de ninguna manera para determinar si estuviesen previamente registrados por terceros, por lo que si tiene cualquier tipo de queja o reclamación que realizar por suplantación o uso indebido de esta información, puede hacerlo en el mail de contacto de esta web: '<i>info.lofc at gmail dot com</i>'. Note que este contacto no está escrito en formato de email para prevenir SPAM.</p>
+<p>Los logos, nombres e imágenes utilizados no han sido contrastados de ninguna manera para determinar si estuviesen previamente registrados por terceros, por lo que si tiene cualquier tipo de queja o reclamación que realizar por suplantación o uso indebido de esta información, puede hacerlo en el <a href="{{ url('/contact') }}">formulario de contacto de esta web</a>.</p>
 
 <p><i>_ English version: _</i></p>
 
 <p>The information contained in this website as well as images, logos and names are merely for entertainment purposes. None of the associations or organizations mentioned on this web are properly registered because they are fictitious.</p>
 
-<p>The logos, names and images used have not been checked in any way to determine if they were previously registered by third parties, so if you have any type of complaint that you make for misrepresentation or misuse of this information, you can do so in the contact email of this web: '<i> info.lofc at gmail dot com </i>'. Note that this contact is not written in an email format to prevent SPAM.</p>
+<p>The logos, names and images used have not been checked in any way to determine if they were previously registered by third parties, so if you have any type of complaint that you make for misrepresentation or misuse of this information, you can do so in the <a href="{{ url('/contact') }}">contact form of this web</a>.</p>
 
 <p>FIFA 16, FIFA 17 et al. and all FIFA assets are property of EA Sports, Electronic Arts and the Fédération Internationale de Football Association (FIFA).</p>
 </div>
