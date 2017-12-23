faf8b86da0289920b0ce544ba5c1b5aea1bb2ca0
Raúl Díez <rauldiez20@gmail.com>
2017-12-23 04:12:59 +0100

    Minor changes. Added tables to 'palmares'.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script src="{{ URL::asset('js/image-picker.min.js') }}" type="text/javascript"></script>
   <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=cobce5u2ujmkx8bm6g0xxrhynbz9tc96j4iieol3h84h3sx6"></script>
-  <script>tinymce.init({ selector:'#palmarestextarea'});</script>
+  <script>tinymce.init({ selector:'#palmarestextarea', plugins: "link,lists,table"});</script>
   <script>tinymce.init({ selector:'#reglamentotextarea', plugins: "link,toc,image,lists,table", image_caption: true});</script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
