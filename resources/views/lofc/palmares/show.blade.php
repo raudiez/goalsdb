fed53783f9c7015eda8339a8c4ccb72b2498441f
Raúl Díez <rauldiez20@gmail.com>
2017-12-30 19:35:58 +0100

    Palmares modified.

--- resources/views/lofc/palmares/show.blade.php
+++ resources/views/lofc/palmares/show.blade.php
@@ -11,14 +11,16 @@
   <h1 class="premier">Palmares</h1>
 </div>
 <div class="row">
-<div class="col-md-1"></div>
-  <div class="col-md-10">
-    <?php echo html_entity_decode($palmaresText) ?>
-  </div>
+  <div class="col-md-1"></div>
+  <div class="col-md-10"></div>
   <div class="col-md-1">
     @if (!Auth::guest())
     <p style="text-align: right;"><a href="{{ url('/lofc/palmares/form') }}" class="btn btn-lofc-primary" role="button" title="Editar texto"><span class="glyphicon glyphicon-pencil"></span></a></p>
     @endif
+  </div>
+  <div class="clearfix" style="margin-top: 90px;"><br/></div>
+  <div class="col-md-12">
+    <?php echo html_entity_decode($palmaresText) ?>
   </div>
 </div>
 @endsection
